c79e8a050145f0d47eb0696f47eaa5075013d8cc
iTzxEduardo <edu-100santos2011@hotmail.com>
2024-05-15 19:20:56 -0300

    15/05

--- exemplo_api/lib/screen.dart
+++ exemplo_api/lib/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'service.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -10,8 +11,10 @@ class WeatherScreen extends StatefulWidget {
 
 class _WeatherScreenState extends State<WeatherScreen> {
   final WeatherService _weatherService = WeatherService(
-    apiKey: '681126f28e7d6fa3a7cfe0da0671e599', // Chave de API para acesso à API de previsão do tempo.
-    baseUrl: 'https://api.openweathermap.org/data/2.5', // URL base da API de previsão do tempo.
+    apiKey:
+        '681126f28e7d6fa3a7cfe0da0671e599', // Chave de API para acesso à API de previsão do tempo.
+    baseUrl:
+        'https://api.openweathermap.org/data/2.5', // URL base da API de previsão do tempo.
   );
 
   late Map<String, dynamic> _weatherData;
@@ -35,53 +38,103 @@ class _WeatherScreenState extends State<WeatherScreen> {
     }
   }
 
-  @override
+  Future<void> _fetchWeatherLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      final weatherData = await _weatherService.getWeatherByLocation(
+          position.latitude, position.longitude);
+      setState(() {
+        _weatherData = weatherData;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Exemplo Weather-API")),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Center(
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _cityController,
-                  decoration: const InputDecoration(labelText: "Digite a Cidade"),
-                  validator: (value) {
-                    if (value!.trim().isEmpty) {
-                      return "Insira a Cidade";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      _fetchWeatherData(_cityController.text);
-                    }
-                  },
-                  child: const Text("Buscar"),
-                ),
-                SizedBox(height: 20),
-                // Show weather information
-                if (_weatherData.isNotEmpty)
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Cidade: ${_weatherData['name']}'), // Exibe o nome da cidade.
-                      Text('Temperatura: ${(_weatherData['main']['temp'] - 273.15).toStringAsFixed(2)} °C'), // Exibe a temperatura em graus Celsius.
-                      Text('Descrção: ${_weatherData['weather'][0]['description']}'), // Exibe a descrição do clima.
-                    ],
-                  ),
-              ],
-            ),
-          ),
+        child: FutureBuilder(
+          future: _fetchWeatherLocation(),
+          builder: (context,snapshot){
+            if(_weatherData.isEmpty){
+              return Center(
+                child: CircularProgressIndicator());
+            }else{
+              return Center(
+                child:Column (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                          'Cidade: ${_weatherData['name']}'), // Exibe o nome da cidade.
+                      Text(
+                          'Temperatura: ${(_weatherData['main']['temp'].toInt - 273.15)} °C'), // Exibe a temperatura em graus Celsius.
+                      Text(
+                          'Descrção: ${_weatherData['weather'][0]['description']}'), // Exibe a descrição do clima.
+                  ],
+                )
+              );
+            }
+          }
         ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: const Text("Exemplo Weather-API")),
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(12),
+  //       child: Center(
+  //         child: Form(
+  //           key: formKey,
+  //           child: Column(
+  //             children: [
+  //               TextFormField(
+  //                 controller: _cityController,
+  //                 decoration:
+  //                     const InputDecoration(labelText: "Digite a Cidade"),
+  //                 validator: (value) {
+  //                   if (value!.trim().isEmpty) {
+  //                     return "Insira a Cidade";
+  //                   } else {
+  //                     return null;
+  //                   }
+  //                 },
+  //               ),
+  //               SizedBox(height: 20),
+  //               ElevatedButton(
+  //                 onPressed: () {
+  //                   if (formKey.currentState!.validate()) {
+  //                     _fetchWeatherData(_cityController.text);
+  //                   }
+  //                 },
+  //                 child: const Text("Buscar"),
+  //               ),
+  //               SizedBox(height: 20),
+  //               // Show weather information
+  //               if (_weatherData.isNotEmpty)
+  //                 Column(
+  //                   crossAxisAlignment: CrossAxisAlignment.start,
+  //                   children: [
+  //                     Text(
+  //                         'Cidade: ${_weatherData['name']}'), // Exibe o nome da cidade.
+  //                     Text(
+  //                         'Temperatura: ${(_weatherData['main']['temp'] - 273.15).toStringAsFixed(2)} °C'), // Exibe a temperatura em graus Celsius.
+  //                     Text(
+  //                         'Descrção: ${_weatherData['weather'][0]['description']}'), // Exibe a descrição do clima.
+  //                   ],
+  //                 ),
+  //             ],
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
