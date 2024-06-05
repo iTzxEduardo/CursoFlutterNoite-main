fd8d41f474e3d64cffa717c12a3ce5d050a3ea04
iTzxEduardo <edu-100santos2011@hotmail.com>
2024-06-04 21:57:06 -0300

    04/06

---
+++ exemplo_firebase/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:exemplo_firebase/firebase_options.dart';
+import 'package:exemplo_firebase/screen/home_screen.dart';
+import 'package:exemplo_firebase/services/login_screen.dart';
+import 'package:exemplo_firebase/services/register_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login':(context) => const LoginScreen(),
+        '/register':(context) => const RegisterScreen(),
+      },
+    );
+  }
+}
---
+++ exemplo_firebase/lib/services/auth_service.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+  //atributos
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //metodos login user
+  Future<User?> loginUsuario(String email, String password) async{
+    try{
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+      return userCredential.user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+
+  }
+
+  //metodo register user
+  Future<User?> registerUsuario(String email, String password) async{
+    try{
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //metodo logout user 
+  Future<void> logoutUsuario() async{
+    try{
+      await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+}
